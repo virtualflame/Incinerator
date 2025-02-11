079c91d3d043cebfd34e8e25d39a3ea488337a42
virtualflame <zbarnes021993@gmail.com>
2025-02-10 23:01:02 -0500

    Added Next.js build configuration

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            "net": false,
+            "tls": false,
+            "fs": false,
+        }
+        return config
+    },
+}
+
+module.exports = nextConfig 
